4e8645a430c74ed3654a0b9f35881fbe0e4fcde4
diegogatica369 <diegogaticaretamales@gmail.com>
2022-10-27 16:31:29 -0300

    first commit

---
+++ Arreglos.java
@@ -0,0 +1,20 @@
+/*Escriba un programa que permita imprimir en pantalla cada uno de los elementos del siguiente
+arreglo: [0, 28, 30, 10, 4]. */
+
+public class Arreglos {
+    public static void main(String [] args) {
+        int [] arreglo = new int [5]; 
+        arreglo[0]=0;
+        arreglo[1]=28;
+        arreglo[2]=30;
+        arreglo[3]=10;
+        arreglo[4]=4;
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+
+    }
+
+}
